8a94d14f55f7e05ea3e3a3ce45163d874227f92c
zmeriksen <zmeriksen@gmail.com>
2019-10-06 20:31:29 -0500

    Started Chapter 4

---
+++ Engine/Texture.swift
@@ -0,0 +1,31 @@
+//
+//  Texture.swift
+//  RetroFPS
+//
+//  Created by Zach Eriksen on 10/6/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import Foundation
+
+public enum Texture: String, CaseIterable {
+    case wall
+}
+
+public struct Textures {
+    private let textures: [Texture: Bitmap]
+}
+
+public extension Textures {
+    init(loader: (String) -> Bitmap) {
+        var textures = [Texture: Bitmap]()
+        for texture in Texture.allCases {
+            textures[texture] = loader(texture.rawValue)
+        }
+        self.init(textures: textures)
+    }
+    
+    subscript(_ texture: Texture) -> Bitmap {
+        return textures[texture]!
+    }
+}
